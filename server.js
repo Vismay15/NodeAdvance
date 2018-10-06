f48d729be318a40426f7ec758c9a2e1f8f79387e
Vismay15 <vis.patel15@gmail.com>
2018-10-06 22:44:55 +0530

    steup startscript and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.set('view engine','hbs');
@@ -17,10 +18,6 @@ app.use((req,res,next)=>{
         }
     })
     next();
-});
-
-app.use((req,res,next)=>{
-    res.render('maintainance.hbs');
 });
 
 app.get('/',(req,res)=>{
@@ -48,6 +45,6 @@ app.get('/about',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000');
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
 });
